b34326e2c07aa9e7e08e784586d059028098d159
hemant17 <hemantbolashetti77@gamail.com>
2025-10-23 15:05:51 +0530

    fahrenheat

--- temp.py
+++ temp.py
@@ -6,6 +6,8 @@ elif temp <= 30:
     status = "Normal"
 else:
     status = "Hot"
-
+    
+fahrenheit = (temp * 9/5) + 32
 print(f"Temperature: {temp}°C")
 print("Status:", status)
+print(f"Temperature in Fahrenheit: {fahrenheit}°F")
